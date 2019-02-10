f88b24af59c60c11926326273bfa7d01e3b1c637
John Rucker <John.Rucker@Solar-Current.com>
2019-02-10 06:54:00 -0600

    first upload

---
+++ app.js
@@ -0,0 +1,37 @@
+const RvrDta =  require('river-data-fetcher');
+const AppMan =  require('app-manager');
+
+const myAppMan = new AppMan();
+
+console.log('__________________ App Config follows __________________');
+console.dir(myAppMan.config, {depth: null});
+console.log('________________________________________________________');
+
+var siteList = {
+    [myAppMan.config.dataSiteCode] : {descripition:[myAppMan.config.descripition]}
+};
+
+var gDta = new RvrDta(siteList);
+
+function dataUpdateInstantValues(){
+    gDta.updateInstantaneousValues(function(errNum, errTxt, dObj){
+        if(errNum == 0){   
+            var rvrLevel = dObj[myAppMan.config.dataLoc][myAppMan.config.dataSiteCode].siteData[myAppMan.config.dataParCode].value;
+            console.log('River Level = ' + rvrLevel);
+
+            if(myAppMan.setGaugeValue(rvrLevel)){
+                myAppMan.setGaugeStatus('Okay, ' + (new Date()).toLocaleTimeString() + ', ' + (new Date()).toLocaleDateString());
+            } else {
+                console.log('Not allowed to send gauge value at this time by AppManager.  Check IOS App for details');
+            };
+        } else {
+            console.log('errNum = ' + errNum);
+            console.log('errTxt = ' + errTxt);
+            myAppMan.setGaugeStatus('Error updating data. ' + (new Date()).toLocaleTimeString() + ', ' + (new Date()).toLocaleDateString() + ' errTxt: ' + errTxt);
+            validData_InstantValues = false;
+        };
+    });
+};
+
+setTimeout(()  =>{dataUpdateInstantValues();}, 5000);                     // wait 5 seconds and then send gauge values (only ran once)
+setInterval(() =>{dataUpdateInstantValues();}, 10 * 60 * 1000);           // every 10 minutes 
